0453a8c6b111578a9ea75f3cf23491e8a48a39b8
Joice Figueiredo <joicebf1995@outlook.com>
2023-08-17 14:29:28 -0300

    pc
    not para pc

--- pratica2-1/main.cpp
+++ pratica2-1/main.cpp
@@ -5,38 +5,72 @@
 #endif
 using namespace std;
 #include <iostream>
+
+float auxX;
+float auxY;
+
+
 static void resize(int width, int height)
 {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+    //gluOrtho2D (-40.0f, 40.0f, -40.0f, 40.0f);
+
+    // estabelece a janela de seleção (esquerda, direita, inferior,
+    // superior) mantendo a proporção com a janela de visualizão
+    if (width <= height)
+        gluOrtho2D (-40.0f, 40.0f, -40.0f*height/width, 40.0f*height/width);
+    else
+        gluOrtho2D (-40.0f*width/height, 40.0f*width/height, -40.0f, 40.0f);
 }
-static void display()
-{
+static void display(){
 //desenha o primeiro ponto
     glPointSize(7.0f);
     glBegin(GL_POINTS);
-    glVertex2i(0, 0);
+    glVertex2i(auxX, auxY);
     glEnd();
     glFlush();
 }
-static void key(unsigned char key, int x, int y){
+static void key(unsigned char key, int x, int y)
+{
+     cout << key << endl;
+    switch(key){
+    case 'R':
+    case 'r':
+        glColor3f(1,0,0);
+        break;
+    case 'b':
+    case 'B':
+        glColor3f(0,0,1);
+        break;
+    case 'G':
+    case 'g':
+        glColor3f(0,1,0);
+        break;
+    case 27:
+        exit(0);
+    break;
+    }
+
     if (key == 27)
         exit(0);
 }
+
 // função para tratar os eventos do mouse
 static void mouse(int button, int state, int x, int y){
     if (button == GLUT_LEFT_BUTTON){
-        if (state == GLUT_DOWN){
-                float auxX = (x/20) -10;
-                float auxY = (-y/20) +10;
-                //float auxX = -10 + ((20/400)*x);
-                //float auxY = 10 - ((20/400)*y);
-                glBegin(GL_POINTS);
-                glVertex2i(auxX,auxY);
-                glEnd();
-                glFlush();
+        if (state == GLUT_DOWN)
+        {
+            auxX = -40 + (x/(400/80)); //((80/400)*x)
+            auxY = 40 - (y/(400/80));//((80/400)*y)
+            display();
             cout << "x; " << x << "y: " << y  << endl;
         }
     }
 }
+
 // função responsável por fazer as inicializações
 void setup(void)
 {
@@ -47,8 +81,9 @@ void setup(void)
 //define a cor preta como inicial
     glColor3f(0.0f, 0.0f, 0.0f);
 
-    gluOrtho2D(-10,10,-10,10);
+    gluOrtho2D(-40,40,-40,40);
 }
+
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
